7ebf4d45e9e41611b0908b55f60b6d568ce8d1ae
dylanquaale <dylanquaale33@gmail.com>
2022-12-20 13:27:26 -0600

    current date and time saved to header

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+let hourNine = $('#hour-9');
+let hourTen = $('#hour-10');
+let hourEleven = $('#hour-11');
+let hourTwelve = $('#hour-12');
+let hourOne = $('#hour-1');
+let hourTwo = $('#hour-2');
+let hourThree = $('#hour-3');
+let hourFour = $('#hour-4');
+let hourFive = $('#hour-5');
+
+// displays cuurent date in header
+let now = dayjs().format("dddd, MMMM D YYYY");
+let displayDate = document.getElementById("currentDay");
+displayDate.innerHTML = now;
+let currentHour = dayjs().format("hh");
+
+
+
+
+
+
 $(function () {
+
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -21,4 +47,9 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
-  
+
+
+
+
+
+ 
